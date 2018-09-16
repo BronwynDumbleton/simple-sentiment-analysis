a3e2c1f2f4d91f91011587f8902600668fb7e15f
BronwynDumbleton <bronwyndumbleton@gmail.com>
2018-09-16 14:01:00 +0200

    README now includes code execution instructions, and virtualenv name corrected

--- README.md
+++ README.md
@@ -7,3 +7,9 @@ To install on Ubuntu run the following:
 chmod u+x install.sh
 ./install.sh
 ```
+
+# How to run the code
+```
+. venv/bin/activate
+python tweet_extractor.py
+```
--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 echo "Creating virtual environment"
 virtualenv --python=/usr/bin/python2.7 venv
 
-. flask/bin/activate
+. venv/bin/activate
 
 echo "Installing requirements with pip"
 pip install -r requirements.txt
